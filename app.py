634b5d406ba6b98948db7d5da3ad0158ccd1ee46
SyrnikovPavel <SyrnikovPavel@gmail.com>
2019-11-22 10:20:47 +0000

    edit filename template

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-def get_html_for_trello(state_dict, template_file='template_tender_trello.html'):
+def get_html_for_trello(state_dict, template_file='/home/SyrnikovPavel/mysite/otc/template_tender_trello.html'):
     """Функция возвращает html данные для отправки"""
     html = open(template_file, encoding='utf8').read()
     template = Template(html)
@@ -24,28 +24,28 @@ def add_new_card(unique_id):
     """
     Функция добавляет карточку с информацией из базы данных
     """
-    
+
     state_dict = get_one_tender(unique_id)
-    
+
     if state_dict['state'].add_trello == False:
-    
+
         client = TrelloClient(
             api_key=api_key,
             api_secret=api_secret,
         )
-        
+
         places = {
             'otc': client.get_label(label_id='5b42f68c9c16fb124a1cd32d', board_id='5b42f68cfa7e02948429e696'),
             'zakupki': client.get_label(label_id='5b42f68c9c16fb124a1cd32b', board_id='5b42f68cfa7e02948429e696'),
             'other': client.get_label(label_id='5b42f68c9c16fb124a1cd329', board_id='5b42f68cfa7e02948429e696'),
             'portal': client.get_label(label_id='5b42f68c9c16fb124a1cd333', board_id='5b42f68cfa7e02948429e696'),
-            'berezka': client.get_label(label_id='5b42f68c9c16fb124a1cd336', board_id='5b42f68cfa7e02948429e696'), 
-            'rts': client.get_label(label_id='5b42f68c9c16fb124a1cd328', board_id='5b42f68cfa7e02948429e696'),   
+            'berezka': client.get_label(label_id='5b42f68c9c16fb124a1cd336', board_id='5b42f68cfa7e02948429e696'),
+            'rts': client.get_label(label_id='5b42f68c9c16fb124a1cd328', board_id='5b42f68cfa7e02948429e696'),
         }
-        
+
         place = state_dict['state'].place
         label = places.get(place)
-            
+
         list_for_new = client.get_list('5b42f6d8787410608050c87e')
 
         card = list_for_new.add_card(
@@ -53,7 +53,7 @@ def add_new_card(unique_id):
             name="Закупка {number}. {name}".format(number=state_dict['state'].id_zak, name=state_dict['state'].name_group_pos),
             desc=get_html_for_trello(state_dict).replace('\\n','\n').replace('\\t','\t'),
         )
-        
+
         members = client.get_board(board_id='5b42f68cfa7e02948429e696').get_members()
         for member in members:
             card.add_member(member)
